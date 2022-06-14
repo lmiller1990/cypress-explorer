54a40e69f7d51559a32b92d9d4f6713acb2773e8
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-06-14 15:39:04 +1000

    chore: docs

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
-# Vue 3 + TypeScript + Vite
+## Cypress Explorer
 
-This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+WIP.
 
-## Recommended IDE Setup
+### Usage
 
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+1. Install the plugin (`cypress.config.ts`)
 
-## Type Support For `.vue` Imports in TS
+```ts
+import { startExplorerServer } from 'cypress-explorer/server'
 
-Since TypeScript cannot handle type information for `.vue` imports, they are shimmed to be a generic Vue component type by default. In most cases this is fine if you don't really care about component prop types outside of templates. However, if you wish to get actual prop types in `.vue` imports (for example to get props validation when using manual `h(...)` calls), you can enable Volar's Take Over mode by following these steps:
+export default defineConfig({
+  e2e: {
+    setupNodeEvents (on, config) {
+      startExplorerServer()
+    }
+  }
+})
+```
 
-1. Run `Extensions: Show Built-in Extensions` from VS Code's command palette, look for `TypeScript and JavaScript Language Features`, then right click and select `Disable (Workspace)`. By default, Take Over mode will enable itself if the default TypeScript extension is disabled.
-2. Reload the VS Code window by running `Developer: Reload Window` from the command palette.
+2. Install the command ('cypress/commands/e2e.ts')
 
-You can learn more about Take Over mode [here](https://github.com/johnsoncodehk/volar/discussions/471).
+```ts
+import { registerCypressExplorer } from 'cypress/explorer'
+
+registerCypressExplorer()
+```
--- plugin/server.ts
+++ plugin/server.ts
@@ -9,51 +9,53 @@ const debug = Debug("cypress-explorer:server");
 
 const PORT = 9991;
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+export function startExplorerServer () {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(cors());
 
-app.post<{}, {}, { projectRoot: string; specs: string[] }>(
-  "/writeSpec",
-  async (req, res) => {
-    const { projectRoot, specs } = req.body;
-    debug("projectRoot: %s specs %o", projectRoot, specs);
+  app.post<{}, {}, { projectRoot: string; specs: string[] }>(
+    "/writeSpec",
+    async (req, res) => {
+      const { projectRoot, specs } = req.body;
+      debug("projectRoot: %s specs %o", projectRoot, specs);
 
-    const e2eDir = path.join(projectRoot, "cypress", "e2e");
-    debug("e2eDir is set to %s", e2eDir);
+      const e2eDir = path.join(projectRoot, "cypress", "e2e");
+      debug("e2eDir is set to %s", e2eDir);
 
-    const imports = specs
-      .filter((spec) => {
-        // do not include "temp" as a spec - we don't want to
-        // import itself, although technically this doesn't seem to
-        // do anything anyway.
-        return !spec.includes("temp.cy.");
-      })
-      .map((spec) => {
-        // assumes temp.cy.js is at cypress/e2e
-        // and other specs are all inside of cypress/e2e
-        // so we need to make them relative to cypress/e2e
-        // TODO: generic enough to handle custom directory, etc.
-        let relative = spec.replace("cypress/e2e/", "");
+      const imports = specs
+        .filter((spec) => {
+          // do not include "temp" as a spec - we don't want to
+          // import itself, although technically this doesn't seem to
+          // do anything anyway.
+          return !spec.includes("temp.cy.");
+        })
+        .map((spec) => {
+          // assumes temp.cy.js is at cypress/e2e
+          // and other specs are all inside of cypress/e2e
+          // so we need to make them relative to cypress/e2e
+          // TODO: generic enough to handle custom directory, etc.
+          let relative = spec.replace("cypress/e2e/", "");
 
-        if (relative.endsWith(".ts")) {
-          relative = relative.slice(0, relative.length - 3);
-        }
+          if (relative.endsWith(".ts")) {
+            relative = relative.slice(0, relative.length - 3);
+          }
 
-        return `import "./${relative}"`;
-      })
-      .join("\n");
+          return `import "./${relative}"`;
+        })
+        .join("\n");
 
-    const tempSpec = path.join(e2eDir, "temp.cy.js");
+      const tempSpec = path.join(e2eDir, "temp.cy.js");
 
-    debug("creating temp spec %s containing %s", tempSpec, imports);
+      debug("creating temp spec %s containing %s", tempSpec, imports);
 
-    await fs.promises.writeFile(tempSpec, imports, "utf-8");
+      await fs.promises.writeFile(tempSpec, imports, "utf-8");
 
-    res.status(200).end();
-  }
-);
+      res.status(200).end();
+    }
+  );
 
-app.listen(PORT, () =>
-  console.log(`[Cypress Explorer]: Listening on port ${PORT}`)
-);
+  app.listen(PORT, () =>
+    console.log(`[Cypress Explorer]: Listening on port ${PORT}`)
+  );
+}
